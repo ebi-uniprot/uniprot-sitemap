9d8d574c28d67495b4e6bc85848efcc415b7a166
Daniel Rice <Daniel.L.Rice@gmail.com>
2022-05-03 13:52:46 +0100

    Add submit-job.sh

---
+++ submit-job.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+bsub \
+-q production-rh74 \
+-J "uniprot-sitemap" \
+-e log.e \
+-o log.o \
+-M 8000 \
+-R"select[mem>2000] rusage[mem=2000] span[hosts=1]" \
+-n 1 \
+./run.sh
