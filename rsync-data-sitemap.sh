6c0c670db726b2edcd28649f35bbc705a4692033
Daniel Rice <dlrice@ebi.ac.uk>
2023-06-02 15:46:26 +0100

    Add comment.

--- rsync-data-sitemap.sh
+++ rsync-data-sitemap.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-become uni_adm
+# Run from codon as uni_adm
+
 rsync -av --delete --include='data-*' --exclude='*' /nfs/production/martin/uniprot/production/sitemap/prod/2023_02/data/ noah-login:/net/isilonP/public/rw/uniprot/uniprot-sitemap/build
 rsync -av --delete --include='data-*' --exclude='*' /nfs/production/martin/uniprot/production/sitemap/prod/2023_02/data/ pg-001:/net/isilonP/public/rw/uniprot/uniprot-sitemap/build
