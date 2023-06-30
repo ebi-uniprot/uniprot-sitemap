2cfa371c63303cfa468db55cb6b28ea8c74a192f
Aurélien Luciani <aurel-l@users.noreply.github.com>
2023-06-30 15:40:45 +0100

    change bash file to sync (effectively making it a template)

--- rsync-data-sitemap.sh
+++ rsync-data-sitemap.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env bash
-
 # Run from codon as uni_adm
 
-rsync -av --delete --include='data-*' --exclude='*' /nfs/production/martin/uniprot/production/sitemap/prod/2023_02/data/ noah-login:/net/isilonP/public/rw/uniprot/uniprot-sitemap/build
-rsync -av --delete --include='data-*' --exclude='*' /nfs/production/martin/uniprot/production/sitemap/prod/2023_02/data/ pg-001:/net/isilonP/public/rw/uniprot/uniprot-sitemap/build
+rsync -av --delete --include='data-*' --exclude='*' /nfs/production/martin/uniprot/production/sitemap/prod/XXXX_XX/data/ noah-login:/net/isilonP/public/rw/uniprot/uniprot-sitemap/build
+rsync -av --delete --include='data-*' --exclude='*' /nfs/production/martin/uniprot/production/sitemap/prod/XXXX_XX/data/ pg-001:/net/isilonP/public/rw/uniprot/uniprot-sitemap/build
